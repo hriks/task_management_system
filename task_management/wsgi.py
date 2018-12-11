5fd5688a32bf123ca1d3ac8ce4b24d8e284e41f2
hriks <hriks@outlook.com>
2018-12-11 23:05:05 +0530

    minor bug changes

--- task_management/wsgi.py
+++ task_management/wsgi.py
@@ -17,6 +17,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from core.settings import WEBSOCKET_URL
 os.environ.update(DJANGO_SETTINGS_MODULE='task_management.settings')
 
+if os.environ.get('PATH_INFO').startswith(WEBSOCKET_URL):
+    print "as"
+else:
+    print "asasasas"
 application = get_wsgi_application()
